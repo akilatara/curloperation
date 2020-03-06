ae9c42dbaa7cc3a87c147384b3c79128a5e1edaf
akilatara <anithasv667@gmail.com>
2020-03-06 13:48:40 +0530

    Curl Operation

---
+++ sample.php
@@ -0,0 +1,28 @@
+<?php
+
+	$htmlContent = file_get_contents("https://www.casa.gov.au/rpa-operator-certificate-holders");
+		
+	$DOM = new DOMDocument();
+	$DOM->loadHTML($htmlContent);
+	
+	$Header = $DOM->getElementsByTagName('th');
+	$Detail = $DOM->getElementsByTagName('td');
+
+    //#Get header name of the table
+	foreach($Header as $NodeHeader) 
+	{
+		$aDataTableHeaderHTML[] = trim($NodeHeader->textContent);
+	}
+	//print_r($aDataTableHeaderHTML); die();
+
+	//#Get row data/detail table without header name as key
+	$i = 0;
+	$j = 0;
+	foreach($Detail as $sNodeDetail) 
+	{
+		$aDataTableDetailHTML[$j][] = trim($sNodeDetail->textContent);
+		$i = $i + 1;
+		$j = $i % count($aDataTableHeaderHTML) == 0 ? $j + 1 : $j;
+	}
+	print_r($aDataTableDetailHTML); die();
+?>
